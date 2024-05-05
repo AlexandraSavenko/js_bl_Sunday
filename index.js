d6c7c3bb9f6ed02bed2db0cfb1ba0b3c52272c39
Nikitin <161200278+Nikitincv@users.noreply.github.com>
2024-05-05 14:58:37 +0200

    ok

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
+// /**
+//  *? Використовуючи функцію if...else,
+//  *? напишіть код, який запитуватиме:
+//  *? "Яка офіційна назва JavaScript?"
+//  *? Якщо користувач вводить "ECMAScript",
+//  *? то показати через alert: "Вірно!"
+//  *? інакше відобразити:"Не знаєте? ECMAScript!"
+//  */
+// const question = prompt("Яка офіційна назва JavaScript?").trim().toLowerCase();
+// if (question === "ecmascript") {
+//   alert("Вірно!");
+// } else {
+//   alert("Не знаєте? ECMAScript!");
+// }
+
+
+
+// /**
+//  *? Напишіть цикл, який виводить у консоль
+//  *? всі непарні числа  від max до min  за спаданням
+//  */
+// let max = 50;
+// const min = 23;
+
+// if (max % 2 === 0) {
+//   max -= 1;
+// }
+// for (let i = max; i >= min; i-= 2) 
+//    {
+//     console.log(i);
+//   }
+
+
 /**
- *? Використовуючи функцію if...else,
- *? напишіть код, який запитуватиме:
- *? "Яка офіційна назва JavaScript?"
- *? Якщо користувач вводить "ECMAScript",
- *? то показати через alert: "Вірно!"
- *? інакше відобразити:"Не знаєте? ECMAScript!"
- */
-const question = prompt("Яка офіційна назва JavaScript?").trim().toLowerCase();
-if (question === "ecmascript") {
-  alert("Вірно!");
-} else {
-  alert("Не знаєте? ECMAScript!");
-}
+Напишіть програму, яка отримає від користувача
+ число (кількість хвилин) та виведе в консоль
+ рядок у форматі годин та хвилин
+ https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+ 70 === 01:10
+*/
 
+const time = prompt("Введіть число");
+// const time = 65;
+const hours = String (Math.floor (time / 60)).padStart(2, 0);
+const minets =String (time % 60).padStart(2, 0);
 
+console.log(`${hours}:${minets}`);
+// console.log(minets);
 
-/**
- *? Напишіть цикл, який виводить у консоль
- *? всі непарні числа  від max до min  за спаданням
- */
-const max = 50;
-const min = 23;
-
-if (max % 2 === 0) {
-  max -= 1;
-}
-for (let i = max; i >= min; i-= 2) 
-   {
-    console.log(i);
-  }
