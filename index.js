f7d79fb54f2491fd98bb38334e8962898303eff2
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-05-19 14:43:38 +0300

    19.05 task -1

--- index.js
+++ index.js
@@ -94,12 +94,34 @@
  *! а не довільний набір символів не потрібно.
  */
 
-let firstNumb = prompt("Введіть число");
+// let firstNumb = prompt("Введіть число");
 
-let total = 0;
+// let total = 0;
 
-while (firstNumb !== null) {
-  total = total + Number(firstNumb);
-  firstNumb = prompt("Введіть число");
+// while (firstNumb !== null) {
+//   total = total + Number(firstNumb);
+//   firstNumb = prompt("Введіть число");
+// }
+// alert(`Загальна сума введених чисел дорівнює ${total}`);
+/**
+ *? Напиши скрипт для об'єкта user,
+ *? послідовно:
+ *? 1 додати поле mood зі значенням 'happy'
+ *? 2 замінить hobby на 'skydiving'
+ *? 3 замінить значення premium на false
+ *? 4 виводить вміст об'єкта users у форматі
+ *? ключ: значення використовуючи Object.keys() і for...of
+ */
+const user = {
+  name: "John",
+  age: 20,
+  hobby: "tennis",
+  premium: true,
+};
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+const userValues = Object.keys(user);
+for (const item of userValues) {
+  console.log(`${item} : ${user[item]}`);
 }
-alert(`Загальна сума введених чисел дорівнює ${total}`);
